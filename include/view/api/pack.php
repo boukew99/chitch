0b42ae12cfef643efec6128f3f341db306f562cb
boukew99 <boukewestra0@gmail.com>
2025-05-22 17:07:32 +0200

    remove build/ directory

--- include/view/api/pack.php
+++ include/view/api/pack.php
@@ -1,5 +1,5 @@
 <?php
-# Chitch © its Maintainers 2025, Licensed under the EUPL
+# © 2025 Chitch-Maintainers, Licensed under the EUPL
 header('Content-Type: text/plain; charset=utf-8');
 chdir('../../../'); # Sets project root as dir
 
@@ -42,11 +42,11 @@ function allOlderRecursive(int $referenceTime, string $dir): bool {
 
 
 
-$zip = build('build/chitch.zip', fn($out, $in) => `(zip --recurse-paths --quiet "$out" $in)` ,'include');
-$project = build('build/chitch.tar.zst' , fn($out, $in) => `tar -caf  $out $in`, 'include license.txt', 'bin');
+$zip = build('bin/chitch.zip', fn($out, $in) => `(zip --recurse-paths --quiet "$out" $in)` ,'include');
+$project = build('bin/chitch.tar.zst' , fn($out, $in) => `tar -caf  $out $in`, 'include license.txt', 'bin');
 
 # Block size of average file size
-$image = build('build/chitch.sqsh', fn($out, $in) => `mksquashfs $in $out -comp zstd -b 256K`, 'include');
+$image = build('bin/chitch.sqsh', fn($out, $in) => `mksquashfs $in $out -comp zstd -b 256K`, 'include');
 
 # Build graph example
 # $lower = build('build/lower.txt', fn($out) => file_put_contents($out, "ok\n"));
--- server.bat
+++ server.bat
@@ -6,4 +6,4 @@ echo Stop server with Ctrl + C
 echo Terminal stay open after stop...
 echo.
 
-bin\php.exe -S localhost:9000 --php-ini include\php.ini --docroot include\view 2> build/server.log
+bin\php.exe -S localhost:9000 --php-ini include\php.ini --docroot include\view 2> bin/server.log
