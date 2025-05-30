26181b42115a04f8ee7d96823110646440ed5cae
boukew99 <boukewestra0@gmail.com>
2025-05-30 14:45:12 +0200

    add multi-platform binary handling for php

--- server.bat
+++ server.bat
@@ -1,9 +1,14 @@
 @echo off
 
 echo.
-echo PHP Dev Server at: http://localhost:9000
-echo Stop server with Ctrl + C
-echo Terminal stay open after stop...
+echo Using PHP binary: bin/php.exe
+echo Development Server started at http://localhost:9000
+echo Stop server with **Ctrl + C**
 echo.
 
+start "" "http://localhost:9000"
+
 bin\php.exe -S localhost:9000 --php-ini include\php.ini --docroot include\view 2> database/server.log
+
+pause
+REM Do not auto-close window
--- server.sh
+++ server.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env sh
 
 ARCH="${ARCH:-$(uname -m)}"
-PHP_BIN="bin/php-$ARCH"
+OS="$(uname -s | tr '[:upper:]' '[:lower:]')"  # linux, darwin, etc
 
-# PHP binary fallback
-[ -x "$PHP_BIN" ] || PHP_BIN="bin/php"
+PHP_BIN="bin/php"         # Generic fallback
+[ -x "$PHP_BIN" ] || PHP_BIN="bin/php-$ARCH"   # Fallback without OS tag
+[ -x "$PHP_BIN" ] || PHP_BIN="bin/php-$ARCH-$OS"
 [ -x "$PHP_BIN" ] || { echo "No valid PHP binary found."; exit 1; }
 
 ADDRESS="${ADDRESS:-localhost:9000}"
 
 echo "Using PHP binary: $PHP_BIN"
-echo "Address: $ADDRESS"
 echo "Development Server started at http://$ADDRESS"
-echo "**Stop server with 'Ctrl + C'**"
+echo "Stop server with **Ctrl + C**"
 echo
 
-# https://www.php.net/manual/en/features.commandline.webserver.php
 export PHP_CLI_SERVER_WORKERS=4
 "$PHP_BIN" \
   --php-ini include/php.ini \
   --server "$ADDRESS" \
   --docroot include/view 2> database/server.log
 
+
 # sudo ADDRESS=0.0.0.0:80 ./server.sh
 # Localarea server for mobile access
+
+# https://www.php.net/manual/en/features.commandline.webserver.php
