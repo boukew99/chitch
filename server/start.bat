8746d3c254b72b3d733421f231e6eecf3810717b
boukew99 <boukewestra0@gmail.com>
2025-06-15 18:08:42 +0200

    rename php/ to server/

--- server/start.bat
+++ server/start.bat
@@ -4,14 +4,14 @@
 cd /d "%~dp0"
 
 echo.
-echo Using PHP binary: bin/php.exe
+echo Using PHP binary: bin/php-x86_64.exe
 echo Development Server started at http://localhost:9000
 echo Stop server with **Ctrl + C**
 echo.
 
 start "" "http://localhost:9000"
 
-bin\x86_64.exe -S localhost:9000 --php-ini .\php.ini --docroot ..\application\view
+bin\php-x86_64.exe -S localhost:9000 --php-ini .\php.ini --docroot ..\application\view
 
 pause
 REM Do not auto-close window
--- server/start.sh
+++ server/start.sh
@@ -6,14 +6,13 @@ cd "$(dirname "$0")"
 ARCH="${ARCH:-$(uname -m)}"
 OS="$(uname -s | tr '[:upper:]' '[:lower:]')"  # linux, darwin, etc
 
-PHP_BIN="bin/$ARCH-$OS"
-[ -x "$PHP_BIN" ] || PHP_BIN="bin/$ARCH"
+PHP_BIN="bin/php-$ARCH-$OS"
+[ -x "$PHP_BIN" ] || PHP_BIN="bin/php-$ARCH"
 [ -x "$PHP_BIN" ] || PHP_BIN="bin/php"         # Generic fallback
-# global PHP
 [ -x "$PHP_BIN" ] || {
   command -v php >/dev/null && PHP_BIN=$(command -v php)
+  # global PHP
 }
-[ -x "$PHP_BIN" ] || (command -v php >/dev/null && PHP_BIN=$(command -v php))
 [ -x "$PHP_BIN" ] || { echo "No valid PHP binary found."; exit 1; }
 
 ADDRESS="${ADDRESS:-localhost:9000}"
@@ -21,7 +20,7 @@ PHP_CONFIG="./php.ini"
 
 echo "Using PHP binary: $PHP_BIN" and config: $PHP_CONFIG
 echo "Development Server started at http://$ADDRESS"
-echo "Outputting logs to php/server.log"
+echo "Outputting logs to server/server.log"
 echo
 
 # Open the server in the default web browser
@@ -41,10 +40,18 @@ export PHP_CLI_SERVER_WORKERS=4
   --server "$ADDRESS" \
   --docroot ../application/view
 
-# sudo ADDRESS=$(hostname -I | awk '{print $1}'):80 php/server.sh
+# sudo ADDRESS=$(hostname -I | awk '{print $1}'):80 server/start.sh
 # Local network server
 
-# php/server.sh > server.log
-# Redirect all output to server.log
+# server/start.sh 2> server/temp/server.log &
+# Redirect all output to server.log and run it in the background
 
 # https://www.php.net/manual/en/features.commandline.webserver.php
+
+
+if [ -t 0 ]; then
+    printf "Press enter to continue...\n"
+    read dummy
+fi
+
+# Don't auto-close window if stdin is terminal
