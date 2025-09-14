d48a18be399753c23b2cfa4317cf7d85e5d8b884
boukew99 <boukewestra0@gmail.com>
2025-09-14 17:06:08 +0200

    moved binaries outside project dir and shortened command names to basename, with shebang replacing type detection.

--- command/log.php
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env php
-See the server log:
-<?php
-$log_file = sys_get_temp_dir() . "/access.log";
-
-(file_exists($log_file)) ? file_get_contents($log_file) : "Log file not found at " . $log_file . PHP_EOL;
